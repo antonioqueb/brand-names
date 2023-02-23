af6f0b8bbf305ff4f6d21519dc24b76044e21b93
Antonio Queb <cardstillo@gmail.com>
2023-02-22 19:51:57 -0600

    update footer component

--- src/components/FooterDisplay/FooterDisplay.tsx
+++ src/components/FooterDisplay/FooterDisplay.tsx
@@ -4,7 +4,11 @@ const FooterDisplay: React.FC = () => {
 
     return (
         <div>
-               Creado por <a href="https://github.com/antonioqueb">Antonio Queb</a>
+            <footer className="text-center  py-3 mt-auto">
+             <p>
+                Creado por <a href="https://github.com/antonioqueb">Antonio Queb</a>
+             </p>
+            </footer>
         </div>
     );
 };
