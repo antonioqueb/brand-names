6789504b8f8094d1b828be4418c6af7478b243bd
querubcastillo <cardstillo@gmail.com>
2023-02-23 12:26:09 -0600

    add DisplayContainer

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -16,7 +16,6 @@ const Home: React.FC = () => {
       <GeneratorDisplay onSubmit={handleNameSubmit} />
       <DisplayContainer brandNames={generatedNames} />
       <FooterDisplay />
-      <br />
 
     </div>
   );
